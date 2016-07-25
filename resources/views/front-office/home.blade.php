96c3e85f3e6f09164c8e3ec3fe53efda4e589ec1
armand-d <a.destais@altimus360.com>
2016-07-25 12:51:27 +0200

    sass bootstrap

---
+++ resources/views/front-office/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title') Accueil @stop
+
+@section('content')
+<div>
+	home 
+</div>
+@stop
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>E-lycée - @yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {!!Html::style('assets/style/stylesheets/screen.css')!!}
+    {!!Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css')!!}
+</head>
+<body>
+<header>
+    @if (Auth::check() && Auth::user()->role == 'teacher')
+        @include('back-office.teacher.partials.header')
+    @elseif (Auth::check() && Auth::user()->role != 'teacher')
+        @include('back-office.student.partials.header')
+    @else
+        @include('front-office.partials.header')
+    @endif
+</header>
+
+<div>
+    <div class="container">@yield('content')</div>
+</div>
+
+<footer>
+    @include('front-office.partials.footer')
+</footer>
+    {!!Html::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js')!!}
+    {!!Html::script('assets/js/script.js')!!}
+
+</body>
+</html>
