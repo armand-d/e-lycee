fe5520c2fe6ef77f1b75083134dc3793a74270bc
armand-d <a.destais@altimus360.com>
2016-07-26 09:44:20 +0200

    af controller

--- app/choice.php
+++ app/choice.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class choice extends Model
+class Choice extends Model
 {
     //
 }
--- app/score.php
+++ app/score.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class score extends Model
+class Score extends Model
 {
     //
 }
