3684fc01ec20f086de42908284f16b92692d1272
armand-d <a.destais@altimus360.com>
2016-07-26 12:44:35 +0200

    af search

---
+++ resources/views/front-office/partials/search.blade.php
@@ -0,0 +1,6 @@
+<div class="content-search">
+	<div class="row">
+		<input type="text" name="q" class="col-lg-20" placeholder="Rechercher...">
+		<button type="submit" class="col-lg-4"><i class="fa fa-search"></i></button>
+	</div>
+</div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,12 +2,17 @@
 <html class="no-js" lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>E-lycée - @yield('title')</title>
+    <title>E-lycée - @yield('title') </title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">  
     {!!Html::style('assets/stylesheets/styles.css')!!}
+    {!!Html::style('assets/stylesheets/animate.css')!!}
+    {!!Html::style('https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css')!!}
+
 </head>
 <body>
+    <div id="no-click-search"></div>
+    @include('front-office.partials.search')
 <header>
     @if (Auth::check() && Auth::user()->role == 'teacher')
         @include('back-office.teacher.partials.header')
@@ -28,6 +33,7 @@
     @include('front-office.partials.footer')
 </footer>
 
+    {!!Html::script('https://code.jquery.com/jquery-3.1.0.min.js')!!}
     <!-- {!!Html::script('assets/javascripts/bootstrap.min.js')!!} -->
     {!!Html::script('assets/javascripts/script.js')!!}
 
