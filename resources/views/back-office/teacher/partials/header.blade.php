86f833855ba8d96ea37332f2528ca2c61a03edf1
armand-d <a.destais@altimus360.com>
2016-07-26 11:26:22 +0200

    af first style header

---
+++ resources/views/back-office/teacher/partials/header.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="col-lg-24">
+				<div class="col-lg-4">logo</div>
+				<div class="col-lg-20"><span>Lycée</span>/ Marcel Gimond</div>
+			</div>
+		</div>
+		<div class="col-lg-12">
+			<div class="col-lg-12 col-lg-offset-12">
+				<div class="col-lg-2">2</div>
+				<div class="col-lg-2">2</div>
+				<div class="col-lg-2">2</div>
+				<div class="col-lg-4">4</div>
+				<div class="col-lg-2">2</div>
+			</div>
+		</div>
+	</div>
+</div>
--- resources/views/front-office/partials/header.blade.php
+++ resources/views/front-office/partials/header.blade.php
@@ -1,6 +1,19 @@
-<img src="" alt="">
-<h1>Lycée</h1>
-<div class="col-lg-17">
-header toto
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-md-12">
+			<div class="col-lg-24 col-md-24">
+				<div class="col-lg-4 col-md-4 logo"></div>
+				<div class="col-lg-20 col-md-20 title"><span>Lycée</span>&nbsp;/&nbsp;Marcel&nbsp;Gimond</div>
+			</div>
+		</div>
+		<div class="col-lg-12 col-md-12">
+			<div class="col-lg-20 col-lg-offset-4 col-md-20 col-md-offset-4">
+				<!-- <div class="col-lg-4 col-md-4"><a href="">Inscription</a></div> -->
+				<div class="col-lg-5 col-md-5 text-center"><a href="">Actualités</a></div>
+				<div class="col-lg-5 col-md-5 text-center"><a href="">Connexion</a></div>
+				<div class="col-lg-5 col-md-5 text-center"><a href="">Rechercher</a></div>
+				<div class="col-lg-5 col-md-5 col-lg-offset-4 col-md-offset-4">facebook</div>
+			</div>
+		</div>
+	</div>
 </div>
-
