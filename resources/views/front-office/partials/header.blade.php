18b60b6444b7bf6d5bf26be16fefba443c05c7bf
QuentinPelluau <quentinpelluau@gmail.com>
2016-08-01 17:52:50 +0200

    pq_front-update-2_link-footer

--- resources/views/front-office/partials/header.blade.php
+++ resources/views/front-office/partials/header.blade.php
@@ -4,7 +4,7 @@
 		@include('front-office.partials.logo')
 		<div class="col-lg-12 col-md-12">
 			<div class="col-lg-20 col-lg-offset-4 col-md-20 col-md-offset-4">
-				<div class="col-lg-5 col-md-5 text-center"><a href="">Actualités</a></div>
+				<div class="col-lg-5 col-md-5 text-center"><a href="{{url('actualites')}}">Actualités</a></div>
 				<div class="col-lg-5 col-md-5 text-center"><a href="" id="login" data-toggle="modal" data-target="#myModal">Connexion</a></div>
 				<div class="col-lg-5 col-md-5 text-center"><a href="" id="btn-search">Rechercher</a></div>
 				<div class="col-lg-5 col-md-5 col-lg-offset-4 col-md-offset-4">
