310c709a3bdaf3f578c448e7b1b7cb560d14cb50
QuentinPelluau <quentinpelluau@gmail.com>
2016-08-02 18:35:46 +0200

    qp_boot3

--- resources/views/front-office/pages/index.blade.php
+++ resources/views/front-office/pages/index.blade.php
@@ -5,16 +5,19 @@
 @section('content')
 <div class="home">
 	<div class="spacer-md"></div>
+
 	<div class="row">
 		<div class="col-sm-6">
 			<div class="spacer-xs"></div>
 			<p class="text-right t-s-2">Le Lycée</p>
 			<p class="text-center bg-dark-grey t-white padding-2 t-s-2 t-light">Marcel <br>Gimond</p>
 		</div>
+
 		<div class="col-sm-12">
 			<p class="row bg-grey padding-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus doloremque voluptatum quis labore a cumque, dignissimos in, quod officia praesentium at hic dolore accusantium sint veritatis ad expedita eligendi fugit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus doloremque voluptatum quis labore a cumque, dignissimos in, quod officia praesentium at hic dolore accusantium sint veritatis ad expedita eligendi fugit
 			</p>
 			<div class="spacer-xs"></div>
+			
 			<div class="row">
 				<div class="line-deco col-sm-8"></div>
 				<div class="col-sm-4"><button class="btn-line t-bold"><a href="{{url('presentation')}}">PR&Eacute;SENTATION</a></button></div>
